1da8d92c2e552ccaf4663ec9f9e90cd680b2cb12
NISSRINELH <elhouarinissrin@gmail.com>
2023-08-10 13:09:51 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
-int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }
+
+/**
+ * main - Entry point
+ *
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+
