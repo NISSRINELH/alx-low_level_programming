814912ba6009f1c6d38cf04179ad3d6dc2a62083
NISSRINELH <elhouarinissrin@gmail.com>
2023-08-11 09:09:16 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,20 +3,20 @@
 #include <time.h>
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-int n;
+float n;
 /* Seed the random number generator */
 srand(time(0));
 /* Generate a random number between -1000 and 1000 */
-n = rand() % 2001 - 1000;
+ n = rand() % (2 * RAND_MAX) - RAND_MAX;
 /* Print the generated number */
-printf("%d", n);
+printf("%f", n);
 /* Check if the number is positive, negative, or zero */
 if (n > 0)
 {
@@ -27,6 +27,7 @@ else if (n < 0)
 printf(" is negative\n");
 }
 else
+
 {
 printf(" is zero\n");
 }
