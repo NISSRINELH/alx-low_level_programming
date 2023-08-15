a0affc026583af85ac5aacac78bfcca6adb356ee
NISSRINELH <elhouarinissrin@gmail.com>
2023-08-15 06:26:13 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -15,11 +15,19 @@ int _putchar(char c)
 return write(1, &c, 1);
 }
 
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
 /**
 * main - Entry point of the program
 *
 * Return: Always 0 (Success)
 */
+
 
 int main(void)
 {
